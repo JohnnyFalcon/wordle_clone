258321ebc4fc6e19ae51b9ba7b3de094dde86643
Johnny <sokolowski8.kuba@gmail.com>
2022-10-27 20:45:56 +0200

    Create API and display random word

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { options } from "./words-options";
 function App() {
+  const [randomWord, setRandomWord] = useState("");
+
+  useEffect(() => {
+    const getWord = async () => {
+      const response = await axios.request(options);
+      setRandomWord(response.data);
+
+      console.log(response.data);
+    };
+
+    getWord();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{randomWord.word}</h1>
     </div>
   );
 }
---
+++ src/words-options.js
@@ -0,0 +1,12 @@
+export const options = {
+  method: "GET",
+  url: "https://wordsapiv1.p.rapidapi.com/words/",
+  params: {
+    letters: "5",
+    random: "true"
+  },
+  headers: {
+    "X-RapidAPI-Key": "8574a3a2e8msh54c1155eff17fb9p1cc7f5jsnf00c1b903a1f",
+    "X-RapidAPI-Host": "wordsapiv1.p.rapidapi.com"
+  }
+};
